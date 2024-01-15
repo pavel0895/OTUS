969184be305f1cb5b7d4bdf3a9a75a4c2e81910c
pavel0895 <pavl171@yandex.ru>
2024-01-15 23:53:51 +0300

    Создал метакласс FigureMeta

--- HW2/src/figure.py
+++ HW2/src/figure.py
@@ -1,9 +1,16 @@
-class Figure:
+class FigureMeta(type):
+    def __new__(cls, name, bases, dct):
+        if "area" not in dct or not isinstance(dct["area"], property):
+            raise ValueError("Класс должен иметь свойство 'area'")
+        return super().__new__(cls, name, bases, dct)
+
+
+class Figure():
     @property
     def area(self):
         raise NotImplementedError()
 
     def add_area(self, figure):
-        if not isinstance (figure, Figure):
+        if not isinstance(figure, Figure):
             raise ValueError("Фигура не является геометрической")
         return self.area + figure.area
