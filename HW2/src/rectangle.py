b8c345670402b97cfb77e4768131ed63db1dafcc
pavel0895 <pavl171@yandex.ru>
2024-01-15 23:55:32 +0300

    Убрал лишние атрибуты

--- HW2/src/rectangle.py
+++ HW2/src/rectangle.py
@@ -1,17 +1,10 @@
 from HW2.src.figure import Figure
 
+
 class Rectangle(Figure):
     def __init__(self, a, b):
         self.__a = a
         self.__b = b
-
-    @property
-    def a(self):
-        return self.__a
-
-    @property
-    def b(self):
-        return self.__b
 
     @property
     def area(self):
