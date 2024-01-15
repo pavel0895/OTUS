5b558e32dc7c05536f0642318f3385a32feac8d3
pavel0895 <76450288+pavel0895@users.noreply.github.com>
2024-01-15 14:03:41 +0300

    Delete HW2/src directory

--- HW2/src/figure.py
+++
@@ -1,9 +0,0 @@
-class Figure:
-    @property
-    def area(self):
-        raise NotImplementedError()
-
-    def add_area(self, figure):
-        if not isinstance (figure, Figure):
-            raise ValueError("Фигура не является геометрической")
-        return self.area + figure.area
